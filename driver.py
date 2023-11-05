e9f1a78fe6018bd4733446c0372e1564300ce853
Gentry Atkinson <gatkinso@stedwards.edu>
2023-11-04 21:51:35 -0500

    Made some orbiting planets

--- driver.py
+++ driver.py
@@ -1,13 +1,24 @@
 import pygame as pg
+from screen import Screen
+
+screen_list = ["navigation"]
+START_SCREEN = "navigation"
 
 if __name__ == "__main__":
     pg.init()
-    screen = pg.display.set_mode((800, 600))
     pg.display.set_caption("Star Trader")
+    screen_display = pg.display.set_mode((1200, 800))
 
     running = True
 
+    screens = {s : Screen(s) for s in screen_list}
+    cur_screen = screens[START_SCREEN]
+
+    clock=pg.time.Clock()
+    
     while running:
+        cur_screen.draw(screen_display, date=0.66)
+        pg.display.update()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
--- screen.py
+++ screen.py
@@ -1,19 +1,40 @@
 import json
 import os
-import event_handler
+import pygame as pg
+from math import sin, cos, pi
+
+from event_handler import Event_Handler
+
+class Planet_Icon:
+    def __init__(self, name, radius, orbit, starting_position) -> None:
+        self.name = name
+        self.radius = radius
+        self.orbital_period = orbit
+        self.starting_position = starting_position
+        self.image = pg.image.load(os.path.join("imgs", name+"_nav_icon.png"))
 
 class Screen:
     def __init__(self, name: str) -> None:
         with open(os.path.join("config", name+".ini"), 'r') as f:
+            print(os.listdir())
             configs = dict(json.load(f))
             self.name = name
             self.background_file = str(configs["background_file"])
-            self.event_handler = event_handler()
+            self.background_image = pg.image.load(os.path.join("imgs", self.background_file+".png"))
+            self.event_handler = Event_Handler()
+            self.selector_file = str(configs["selector_file"])
+            self.selector_image = pg.image.load(os.path.join("imgs", self.selector_file+".png"))
 
+            self.planet_icons = [
+                Planet_Icon("Sun", 0, 1, (600, 400)),
+                Planet_Icon("Earth", 200, 1, 0)
+            ]
 
-if __name__ == "__main__":
-    config_dic = {
-        "background_file" : "navigation_screen.png"
-    }
-    with open("config/navigation.ini", 'w+') as f:
-        f.write(json.dumps(config_dic))
+            
+
+    def draw(self, screen: pg.Surface, date = 0) -> None:
+        screen.blit(self.background_image, (0,0))
+        for icon in self.planet_icons:
+            pos_x = 600 + icon.radius * sin(2*pi*(date % icon.orbital_period)/icon.orbital_period)
+            pos_y = 400 + icon.radius * cos(2*pi*(date % icon.orbital_period)/icon.orbital_period)
+            screen.blit(icon.image, (pos_x - icon.image.get_width(), pos_y - icon.image.get_height()))
