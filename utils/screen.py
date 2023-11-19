f5e3feb9e74640031b02bd36906787dc0d678d59
Gentry Atkinson <gatkinso@stedwards.edu>
2023-11-19 17:39:55 -0600

    screens can override parent draw and update funcs

--- utils/screen.py
+++ utils/screen.py
@@ -1,5 +1,6 @@
 import json
 import os
+from math import sqrt
 
 import pygame as pg
 
@@ -43,8 +44,8 @@ class Screen:
 
     def draw(self, screen: pg.Surface, date = 0) -> None:
         screen.blit(self.background_image, (0,0))
-        for name, icon in self.icons.items():
-            screen.blit(icon.image, icon.pos(date))
+        for _, icon in self.icons.items():
+            screen.blit(icon.image, icon.pos())
         if screen_status.focus:
             Screen._draw_on_center(screen_status.focus_icon, self.selector_image, screen, date)
 
@@ -52,18 +53,37 @@ class Screen:
         # new_p.star_date += 0.001
         global screen_status
         p, screen_status = self.key_handler.process_keys(p, screen_status)
-        if screen_status.focus:
-            screen_status.focus_icon = self.icons[screen_status.focus]
         return p
     
 class NavScreen(Screen):
     def __init__(self) -> None:
         super().__init__("navigation")
-        self.icons["Sun"] = Planet_Icon("Sun", 0, 1, (600, 400))
-        self.icons["Earth"] = Planet_Icon("Earth", 200, 1, 0)
-        self.icons["Venus"] = Planet_Icon("Venus", 100, 0.615, 0.5)
-        self.icons["Mars"] = Planet_Icon("Mars", 400, 1.88, 0.75)
+        self.planet_icons = {}
+        self.planet_icons["Sun"] = Planet_Icon("Sun", 0, 1, (600, 400))
+        self.planet_icons["Earth"] = Planet_Icon("Earth", 200, 1, 0)
+        self.planet_icons["Venus"] = Planet_Icon("Venus", 100, 0.615, 0.5)
+        self.planet_icons["Mars"] = Planet_Icon("Mars", 400, 1.88, 0.75)
         self.key_handler = NavKeyHandler()
+
+    def get_travel_time(a: Planet_Icon, b: Planet_Icon):
+        a_x, a_y = a.pos()
+        b_x, b_y = b.pos()
+        d_x = abs(a_x - b_x)
+        d_y = abs(a_y - a_y)
+        return TRAVEL_TIME_PER_PIXEL * sqrt(d_x**2 + d_y**2)
+    
+    def draw(self, screen: pg.Surface, date = 0) -> None:
+        super().draw(screen, date)
+        for _, icon in self.planet_icons.items():
+            screen.blit(icon.image, icon.pos(date))
+
+    def update(self, p: Player) -> Player:
+        global screen_status
+        p = super().update(p)
+        if screen_status.focus:
+            screen_status.focus_icon = self.planet_icons[screen_status.focus]
+        return p
+
 
 class CockpitScreen(Screen):
     def __init__(self) -> None:
